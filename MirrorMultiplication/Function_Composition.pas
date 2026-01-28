40b01ba7d2aedc31773d5d048d866749d774f9f2
BENMABROUK MARIYA <om_benmabrouk@esi.dz>
2026-01-29 00:01:34 +0100

    Add files via upload

--- MirrorMultiplication/Function_Composition.pas
+++ MirrorMultiplication/Function_Composition.pas
@@ -1,1 +1,16 @@
 
+
+Function Comp(A:Integer;B:Integer): Integer;
+Var
+  X,Y,R: Integer;
+Begin
+  R := B;
+  X := A;
+  While X>0 Do
+    Begin
+      Y := X Mod 10;
+      X := X Div 10;
+      R := R*10+Y;
+    End;
+  comp := R;
+End;
